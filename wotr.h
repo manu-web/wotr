feff0f760b399c998a2e2accda80b07887ba196e
John Shawger <shawgerj@cs.wisc.edu>
2022-12-01 15:53:46 -0600

    initial commit. initial cmake

---
+++ wotr.h
@@ -0,0 +1,24 @@
+#ifndef WOTR_H
+#define WOTR_H
+
+#include <vector>
+
+typedef struct {
+    size_t ksize;
+    size_t vsize;
+} item_header;
+
+class Wotr {
+public:
+  Wotr (const char* logname);
+
+  size_t CurrentOffset();
+  int WotrWrite(std::string& logdata);
+  int WotrGet(size_t offset, char** data, size_t* len);
+
+private:
+    std::string _logname;
+    int _log; // fd 
+};
+
+#endif // WOTR_H
